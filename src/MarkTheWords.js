62ad4aab2b1529dbd5d4d46913f5844e2ec8fcba
ejm5763 <ejm5763@gmail.com>
2021-11-18 13:10:26 -0500

    back-end basically done?

--- src/MarkTheWords.js
+++ src/MarkTheWords.js
@@ -1,6 +1,5 @@
 // dependencies / things imported
 import { LitElement, html, css } from 'lit';
-import { LitElement, css, html } from "https://unpkg.com/lit@2.0.2/index.js?module";
 
 // EXPORT (so make available to other documents that reference this file) a class, that extends LitElement
 // which has the magic life-cycles and developer experience below added
@@ -13,6 +12,14 @@ export class MarkTheWords extends LitElement {
   // HTMLElement life-cycle, built in; use this for setting defaults
   constructor() {
     super();
+    this.selectedWord = '';
+    this.wordList = [];
+    this.answers = "";
+    this.correctAnswers = [];
+    //parse text
+    this.wordArray = this.pgh.split(/\s+/g);
+    this.pgh = "";
+
   }
 
   // properties that you wish to use as data in HTML, CSS, and the updated life-cycle
@@ -28,10 +35,28 @@ export class MarkTheWords extends LitElement {
   // this allows you to react to variables changing and use javascript to perform logic
   updated(changedProperties) {
     changedProperties.forEach((oldValue, propName) => {
-      
+      if (propName === "wordArray") {
+        this.rebuildContents(this[propName]);
+      }
+      if (propName === "answers" && this[propName]) {
+        this.correctAnswers = this[propName].split(",");
+        this.answers = null;
+      }
     });
   }
 
+  rebuildContents(ary) {
+    //wipe out inner
+    this.shadowRoot.querySelector("#textArea").innerHTML = "";
+    ary.forEach((word) => {
+      let span = document.createElement("span");
+      span.innerText = word;
+      span.setAttribute("tabindex", "-1");
+      span.addEventListener("click", this.clickWord.bind(this));
+
+      this.shadowRoot.querySelector("#textArea").appendChild(span);
+    });
+  }
   // Lit life-cycle; this fires the 1st time the element is rendered on the screen
   // this is a sign it is safe to make calls to this.shadowRoot
   firstUpdated(changedProperties) {
@@ -54,19 +79,102 @@ export class MarkTheWords extends LitElement {
 
   // CSS - specific to Lit
   static get styles() {
-    return css`
-      :host {
-        display: block;
+    return [css`
+    :host {
+      display: block;
+      padding: 16px;
+      margin: 16px;
+      border: 2px solid black;
+    }
+    span {
+      display: inline-flex;
+      font-size: var(--x-ample-font-size, 24px);
+      padding: 2px;
+      margin: 0 2px;
+    }
+    span:hover,span:focus
+    {
+      outline: 2px dashed blue;
+      cursor: pointer;        
+    }
+    span[data-selected] {
+      outline: 2px solid purple;
+    }
+    span[data-selected]:hover,
+    span[data-selected]:focus
+    {
+      outline: 2px solid blue;
+    }
+    span[data-status="correct"] {
+      outline: 2px solid green;
+    }
+    span[data-status="correct"]::after {
+      content: "+1";
+      font-size: 14px;
+      color: green;
+      font-weight: bold;
+      border-radius: 50%;
+      border: 2px solid green;
+      padding: 4px;
+      margin-left: 8px;
+      line-height: 14px;
+    }
+    span[data-status="incorrect"] {
+      outline: 2px solid red;
+    }
+    span[data-status="incorrect"]::after {
+      content: "-1";
+      font-size: 14px;
+      color: red;
+      font-weight: bold;
+      border-radius: 50%;
+      border: 2px solid red;
+      padding: 4px;
+      margin-left: 8px;
+      line-height: 14px;
+    }
+    .buttons,.correct {
+      margin: 8px;
+    }
+  `];
+  }
+
+  selectWord(e) {
+    const el = e.target;
+    if (el.getAttribute("data-selected")) {
+      el.removeAttribute("data-selected")
+    }
+    else {
+      el.setAttribute("data-selected", "data-selected");
+    }
+  }
+
+  checkAnswer(e) {
+    const selected = this.shadowRoot.querySelectorAll("#textArea span[data-selected]");
+    console.log(selected);
+    for (var i = 0; i < selected.length; i++) {
+      const el = selected[i];
+      if (this.correctAnswers.includes(el.innerText)) {
+        el.setAttribute("data-status", "correct");
       }
-    `;
+      else {
+        el.setAttribute("data-status", "incorrect");
+      }
+    }
   }
 
   // HTML - specific to Lit
   render() {
     return html`
-      <h1>Make me awesome</h1>
-      <p>Build the future we.</p>
-      <slot></slot>
+      <div id="textArea">
+        <div class = "buttons">
+          <button @click="${this.checkAnswer}">Check</button>
+      </div>
+      <div class="correct">
+        <h1>Correct Answers (in .map)</h1>
+        <ul> ${this.correctAnswers.map((item, i) => html`<li data-index="${i}">${item}</li>`)}
+    </ul>
+    </div>
     `;
   }
 
